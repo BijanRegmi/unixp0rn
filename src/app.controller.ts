f4fbb2580aacca5c6e6735d73d3e0547367e8b38
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2024-06-08 10:41:00 +0545

    feat: list api with filters

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,26 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
 import { PopulateService } from './populate.service';
 import * as fs from 'fs';
 import { Entry } from './type';
+import { SortFields, SortOrder } from './dto/listentries.dto';
 
 @Controller('app')
 export class AppController {
-  constructor(private populateService: PopulateService) {}
+  constructor(
+    private appService: AppService,
+    private populateService: PopulateService,
+  ) {}
+
+  @Get()
+  list() {
+    return this.appService.list({
+      skip: 0,
+      take: 10,
+      sort: { field: SortFields.REACTION_COUNT, order: SortOrder.ASC },
+      filter: { reactionCount: { between: { start: 50, end: 100 } } },
+    });
+  }
 
   @Post()
   async populate() {
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,68 @@
 import { Injectable } from '@nestjs/common';
-import { ListBodyDto, SortFields, SortOrder } from './dto/listentries.dto';
-import { DataSource } from 'typeorm';
+import { ListBodyDto, SortFields } from './dto/listentries.dto';
+import {
+  Between,
+  DataSource,
+  FindOptionsWhere,
+  ILike,
+  LessThan,
+  LessThanOrEqual,
+  MoreThan,
+  MoreThanOrEqual,
+} from 'typeorm';
+import { Entry } from './entities/entry.entity';
 
 @Injectable()
 export class AppService {
   constructor(private dataSource: DataSource) {}
+
+  async list(options: ListBodyDto) {
+    const { sort, filter } = options;
+
+    const whereClause: FindOptionsWhere<Entry> = {};
+
+    if (filter?.authorId) {
+      whereClause.authorId = filter.authorId;
+    }
+
+    if (filter?.content) {
+      whereClause.content = ILike(`%${filter.content}%`);
+    }
+
+    if (filter?.contentType) {
+      whereClause.attachments = { type: filter.contentType };
+    }
+
+    if (filter?.reactionCount) {
+      if (filter.reactionCount.eq) {
+        whereClause.reactionCount = filter.reactionCount.eq;
+      } else if (filter.reactionCount.gt) {
+        whereClause.reactionCount = MoreThan(filter.reactionCount.gt);
+      } else if (filter.reactionCount.lt) {
+        whereClause.reactionCount = LessThan(filter.reactionCount.lt);
+      } else if (filter.reactionCount.gte) {
+        whereClause.reactionCount = MoreThanOrEqual(filter.reactionCount.gte);
+      } else if (filter.reactionCount.lte) {
+        whereClause.reactionCount = LessThanOrEqual(filter.reactionCount.lte);
+      } else if (filter.reactionCount.between) {
+        whereClause.reactionCount = Between(
+          filter.reactionCount.between.start,
+          filter.reactionCount.between.end,
+        );
+      }
+    }
+
+    const entries = await this.dataSource.getRepository(Entry).find({
+      where: whereClause,
+      skip: options.skip,
+      take: options.take,
+      order: !sort
+        ? undefined
+        : sort?.field === SortFields.REACTION_COUNT
+        ? { reactionCount: sort.order }
+        : { timestamp: sort.order },
+      relations: { author: true, reactions: true, attachments: true },
+    });
+    return entries;
+  }
 }
