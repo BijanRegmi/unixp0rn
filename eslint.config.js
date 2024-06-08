bdc273723699c7f227dc6feaea1e7fa39691649c
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2024-06-09 01:03:04 +0545

    feat: prettier config added to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,14 @@
 const tsEslint = require('typescript-eslint');
 const eslint = require('@eslint/js');
+const eslintConfigPrettier = require('eslint-config-prettier');
 
 module.exports = [
-  eslint.configs.recommended,
-  ...tsEslint.configs.recommended,
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+    eslint.configs.recommended,
+    ...tsEslint.configs.recommended,
+    {
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'off',
+        },
     },
-  },
+    eslintConfigPrettier,
 ];
