d763893d19cf08e72c655363dff4538e75da9bf6
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2024-06-10 22:16:50 +0545

    chore: setup frontend

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 const tsEslint = require('typescript-eslint');
 const eslint = require('@eslint/js');
 const eslintConfigPrettier = require('eslint-config-prettier');
+const reactRefresh = require('eslint-plugin-react-refresh');
 
 module.exports = [
     eslint.configs.recommended,
@@ -9,6 +10,17 @@ module.exports = [
         rules: {
             '@typescript-eslint/no-explicit-any': 'off',
         },
+    },
+    {
+        files: ['./apps/frontend/**/*.ts', './apps/frontend/**/*.tsx'],
+        env: { browser: true, es2020: true },
+        rules: {
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+        },
+        ...reactRefresh,
     },
     eslintConfigPrettier,
 ];
