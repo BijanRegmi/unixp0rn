a8bc4125712bcfc140749e3bfd6eff8fb2c45d42
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2024-06-29 16:44:51 +0545

    chore: add logger

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
 
   const port = app.get(ConfigService).get('port');
+  Logger.log(`=> Starting application at port: ${port}`);
   await app.listen(port || 3000);
 }
 bootstrap();
